0339b638781d2f44089b63423dc93c5e5acbfe1e
Coordenada Tecnologia <ofccoordenada@gmail.com>
2024-11-11 08:50:12 -0400

    Update animação.js

--- animação.js
+++ animação.js
@@ -32,3 +32,39 @@ setInterval(showNextService, 5000);
 
 // Exibe o primeiro serviço ao carregar a página
 showService(currentServiceIndex);
+
+const frases = [
+    "SEU GUIA NO MARKETING DIGITAL",
+    "A EXTENSÃO DA SUA EMPRESA",
+    "SUA AGÊNCIA DE MARKETING"
+  ];
+  
+  let fraseIndex = 0;
+  let charIndex = 0;
+  const typingSpeed = 30; // Velocidade de digitação em milissegundos
+  const textoVariavel = document.querySelector(".typing-text");
+  
+  function digitar() {
+    if (charIndex < frases[fraseIndex].length) {
+      textoVariavel.textContent += frases[fraseIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(digitar, typingSpeed); // Chama a função de digitação
+    } else {
+      setTimeout(apagar, 2000); // Espera 2 segundos antes de apagar
+    }
+  }
+  
+  function apagar() {
+    if (charIndex > 0) {
+      textoVariavel.textContent = frases[fraseIndex].substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(apagar, typingSpeed);
+    } else {
+      fraseIndex = (fraseIndex + 1) % frases.length; // Vai para a próxima frase
+      setTimeout(digitar, typingSpeed);
+    }
+  }
+  
+  // Inicia o efeito de digitação
+  digitar();
+  
